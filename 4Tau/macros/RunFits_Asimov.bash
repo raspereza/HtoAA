44b7ae5ce0250a1aab7fa481f377397dc459b6d3
Aliaksei Raspiareza <rasp@mail.desy.de>
2021-09-26 19:37:36 +0200

    scripts to run fits

---
+++ 4Tau/macros/RunFits_Asimov.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# $1 : mass
+# $2 : mu
+combine -M FitDiagnostics --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL  --robustFit 1 haa_2018-13TeV_ma${1}.txt --expectSignal ${2} -t -1 --rMin=-10 --rMax=10 -m ${1} 
+mv fitDiagnostics.root fitDiagnostics_ma${1}.root
+

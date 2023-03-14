1c6f13087c167a20b51ae29b80fea594dca309f2
lakshmip2609 <92534901+lakshmip2609@users.noreply.github.com>
2023-03-14 11:43:54 +0100

    Delete 4Tau/test/2016 directory

--- 4Tau/test/2016/2016_postVFP/RunData_2016.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in F G H
-do
-    echo submitting jobs for sample DoubleMuon_Run2016${i}
-    ./HTC_submit_seq.sh analysis_macro_4tau_2016 analysisMacro_2016.conf DoubleMuon_Run2016${i} 100
-done
--- 4Tau/test/2016/hadd_mc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/csh
-rm $1.root 
-hadd $1.root ./2016_preVFP/$1_files/*.root ./2016_postVFP/$1_files/*.root
-

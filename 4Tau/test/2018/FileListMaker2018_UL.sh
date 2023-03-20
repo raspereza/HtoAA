62f4c52b62c2530309243c3dd1f97e53a11c7502
lakshmip2609 <92534901+lakshmip2609@users.noreply.github.com>
2023-03-20 12:50:38 +0100

    Update FileListMaker2018_UL.sh

--- 4Tau/test/2018/FileListMaker2018_UL.sh
+++ 4Tau/test/2018/FileListMaker2018_UL.sh
@@ -4,37 +4,34 @@
 for i in A B C D
 do
     echo creating file list for data sample DoubleMuon_Run2018${i}
-    ls /pnfs/desy.de/cms/tier2/store/user/rasp/ntuples/H2AA/2018/data/DoubleMuon_Run2018${i}/*root > DoubleMuon_Run2018${i}
+    ls /pnfs/desy.de/cms/tier2/store/user/lsreelat/NTuples/2018/HtoAA/DoubleMuon/DoubleMuon-Run2018${i}-UL2018/*root > DoubleMuon_Run2018${i}
 done
 
-#for i in A B C D
-#do
-#    echo creating file list for data sample SingleMuon_Run2018${i}
-#    ls /pnfs/desy.de/cms/tier2/store/user/rasp/ntuples/H2AA/2018/data/SingleMuon_Run2018${i}/*root > SingleMuon_Run2018${i}
-#done
-
-#File lists for 2018 MC ntuples v2:
-echo creating file lists for HToAA_AToMuMu_AToTauTau samples
-for i in 3p6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 
-do
-    ls /nfs/dust/cms/user/consuegs/ntuples/NMSSM_2018_v2/'SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-125_M-'$i''/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-$i
-done
-
+#File lists for 2018 UL MC signal samples:
 echo creating file lists for HToAA_To4Tau samples
 for i in {4..21}
-do
-    ls /pnfs/desy.de/cms/tier2/store/user/sconsueg/ntuples/H2aa_4tau/2018/mc/SUSYGluGluToHToAA_AToTauTau_M-125_M-${i}/*root > SUSYGluGluToHToAA_AToTauTau_M-125_M-${i}
-    ls /pnfs/desy.de/cms/tier2/store/user/sconsueg/ntuples/H2aa_4tau/2018/mc/SUSYVBFToHToAA_AToTauTau_M-125_M-${i}/*root > SUSYVBFToHToAA_AToTauTau_M-125_M-${i}
-    ls /pnfs/desy.de/cms/tier2/store/user/sconsueg/ntuples/H2aa_4tau/2018/mc/SUSYVH_HToAA_AToTauTau_M-125_M-${i}/*root > SUSYVH_HToAA_AToTauTau_M-125_M-${i}
-    ls /pnfs/desy.de/cms/tier2/store/user/sconsueg/ntuples/H2aa_4tau/2018/mc/SUSYttH_HToAA_AToTauTau_M-125_M-${i}/*root > SUSYttH_HToAA_AToTauTau_M-125_M-${i}
+do 
+	ls /pnfs/desy.de/cms/tier2/store/user/lsreelat/NTuples/2018/HtoAA/SUSYGluGluToHToAA_AToMuMu_AToTauTau/SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-125_M-${i}/*root > SUSYGluGluToHToAA_AToMuMu_AToTauTau_M-125_M-${i}
+	ls /pnfs/desy.de/cms/tier2/store/user/lsreelat/NTuples/2018/HtoAA/SUSYGluGluHToAA_AToTauTau/SUSYGluGluHToAA_AToTauTau_M-125_M-${i}/*root > SUSYGluGluToHToAA_AToTauTau_M-125_M-${i}
+	ls /pnfs/desy.de/cms/tier2/store/user/lsreelat/NTuples/2018/HtoAA/SUSYVBFHToAA_AToTauTau/SUSYVBFHToAA_AToTauTau_M-125_M-${i}/*root > SUSYVBFToHToAA_AToTauTau_M-125_M-${i}
+	ls /pnfs/desy.de/cms/tier2/store/user/lsreelat/NTuples/2018/HtoAA/SUSYVHToAA_AToTauTau/SUSYVHToAA_AToTauTau_M-125_M-${i}/*root > SUSYVH_HToAA_AToTauTau_M-125_M-${i}
+	ls /pnfs/desy.de/cms/tier2/store/user/lsreelat/NTuples/2018/HtoAA/SUSYttHToAA_AToTauTau/SUSYttHToAA_AToTauTau_M-125_M-${i}/*root > SUSYttH_HToAA_AToTauTau_M-125_M-${i}
 done
 
-# TTSemileptonic sample should be splitted (long list for ls command)
-echo "Creating file list for sample TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8"
-    ls /pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/Oktoberfest21/2018/mc/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*0.root > TTToSemiLeptonic
+# TTSemileptonic sample should be split (long list for ls command)
+echo "Creating file list for sample TTToSemiLeptonic"
+    ls /pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/Oktoberfest21/2018/mc/TTToSemiLeptonic/*0.root > TTToSemiLeptonic
 for index in {1..9}
 do
-    ls /pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/Oktoberfest21/2018/mc/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/*${index}.root >> TTToSemiLeptonic
+    ls /pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/Oktoberfest21/2018/mc/TTToSemiLeptonic/*${index}.root >> TTToSemiLeptonic
+done
+
+# WJetsToLNu sample should be split (long list for ls command)
+echo "Creating file list for sample WJetsToLNu"
+    ls /pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/Oktoberfest21/2018/mc/WJetsToLNu/*0.root > WJetsToLNu
+for index in {1..9}
+do
+    ls /pnfs/desy.de/cms/tier2/store/user/acardini/ntuples/Oktoberfest21/2018/mc/WJetsToLNu/*${index}.root >> WJetsToLNu
 done
 
 #File lists for VV background MC samples
@@ -49,15 +46,14 @@ ZZ_13TeV-pythia8
 )
 
 #File lists for background MC samples
-samples=(DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8
-DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_ext
-ST_t-channel_antitop_4f_InclusiveDecays_TuneCP5_erdON_13TeV-powheg-madspin-pythia8
-ST_t-channel_top_4f_InclusiveDecays_TuneCP5_erdON_13TeV-powheg-madspin-pythia8
-ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8
-ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8
-TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8
-TTToHadronic_TuneCP5_13TeV-powheg-pythia8
-WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8
+samples=(DYJetsToLL_M-10to50
+DYJetsToLL_M-50
+ST_t-channel_antitop_4f
+ST_t-channel_top_4f
+ST_tW_top_5f_inclusiveDecays
+ST_tW_antitop_5f_inclusiveDecays
+TTTo2L2Nu
+TTToHadronic
 )
 
 names=(DYJetsToLL_M-10to50
@@ -68,7 +64,6 @@ ST_tW_top
 ST_tW_antitop
 TTTo2L2Nu
 TTToHadronic
-WJetsToLNu
 )
 
 names_QCD=(QCD_Pt-15To20_MuEnrichedPt5
@@ -100,7 +95,7 @@ while [ $k -lt ${#samples_VV[@]} ]
 do
     echo "Creating file list for sample" ${samples_VV[$k]} 
 
-    ls /pnfs/desy.de/cms/tier2/store/user/mmeyer/ntuples/2018/mc/${samples_VV[$k]}*/*root > ${names_VV[$k]}
+    ls /pnfs/desy.de/cms/tier2/store/user/lsreelat/NTuples/2018/HtoAA/VV_inclusive/${samples_VV[$k]}*/*root > ${names_VV[$k]}
       
     k=`expr $k + 1` 
 done
@@ -113,4 +108,3 @@ do
       
     j=`expr $j + 1` 
 done
-
